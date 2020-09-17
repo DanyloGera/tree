ebfa7386b65f9297e0f431d8552e49bca1f8b06f
DanyloGera <64353215+DanyloGera@users.noreply.github.com>
2020-09-17 19:49:57 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var height = prompt("Яка довжина ялинки?")
+height = Number(height)
+
+  var message =
+      height <= 0 ? "Довжина має бути додатнім числом!" :
+      height = 1 ? "Довжина замала!" :
+      height > 1 ? document.img.src = 'https://hromadske.ck.ua/wp-content/uploads/2019/11/yalynka2.jpg';
+      ""
+  alert(message);
+
+// if (height > 1)
+//   document.img.src = 'https://hromadske.ck.ua/wp-content/uploads/2019/11/yalynka2.jpg';
