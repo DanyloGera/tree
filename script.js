37249da511fb783ac8799bc8980d5465c599e1de
DanyloGera <64353215+DanyloGera@users.noreply.github.com>
2020-09-26 08:24:15 +0300

    1

--- script.js
+++ script.js
@@ -4,9 +4,17 @@ height = Number(height)
   var message =
       height <= 0 ? "Довжина має бути додатнім числом!" :
       height = 1 ? "Довжина замала!" :
-      height > 1 ? document.img.src = 'https://hromadske.ck.ua/wp-content/uploads/2019/11/yalynka2.jpg';
+      height > 1 ? document.body.innerHTML+="</br>";
+         document.body.innerHTML+="&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*</br>";
+         document.body.innerHTML+="&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;**</br>";
+         document.body.innerHTML+="&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;***</br>";
+         document.body.innerHTML+="&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;****</br>";
+         document.body.innerHTML+="&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*****</br>";
+         document.body.innerHTML+="&nbsp;&nbsp;&nbsp;&nbsp;******</br>";
+         document.body.innerHTML+="&nbsp;&nbsp;&nbsp;*******</br>";
+         document.body.innerHTML+="&nbsp;&nbsp;********</br>";
+         document.body.innerHTML+="&nbsp;*********</br>";
+         document.body.innerHTML+="**********</br>";
+
       ""
   alert(message);
-
-// if (height > 1)
-//   document.img.src = 'https://hromadske.ck.ua/wp-content/uploads/2019/11/yalynka2.jpg';
